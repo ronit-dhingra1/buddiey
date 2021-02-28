66475f00b62f0b32073a08f6a2da15204e9a2283
Ethan Howell <ethan@buddiey.live>
2021-02-28 15:11:04 -0500

    First commit

--- buddiey/__init__.py
+++ buddiey/__init__.py
@@ -3,7 +3,7 @@ from .main.path import main
 from .extensions import mongo
 import os
 
-def create_app(test_config=None):
+def create_app(test_config=None): #Hello
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
